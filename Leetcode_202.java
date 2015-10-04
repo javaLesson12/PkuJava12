0fcbd4614568653fad6105d31af9a0f7d9fb612d
javaLesson12 <2444574046@qq.com>
2015-10-04 23:15:53 +0800

    yu-0-6

--- Leetcode_202.java
+++ Leetcode_202.java
@@ -1,29 +1,24 @@
-/**
- * Created by umic-lord on 2015/9/25.
- */
-public class Leetcode_202 {
-//尝试了多个数，凡是无限循环的，这个循环数列肯定包含145。
+public class Solution {
+    //尝试了多个数，凡是无限循环的，这个循环数列肯定包含145。
     public boolean isHappy(int n) {
-        if (n == 1) return true;//1必是
-        if (n == 0) return false;//0必不是
+        if (n == 1) return true;
+        if (n == 0) return false;
         int on = n;
         int i=0;
         while (true) {
             int ans = 0;
             while (n > 0) {
-                ans += Math.pow(n % 10, 2);//依题目进行处理
+                ans += Math.pow(n % 10, 2);
                 n /= 10;
             }
-            if (on == ans||ans==145) {//等于自己原本数或等于145，必是
+            if (ans == 4 || ans == 16 || ans == 20 || 
+                ans == 37 || ans == 42 || ans == 58 || 
+                ans == 89 || ans == 145) {//等于4，16，20，37，42，58，89，145，必是
                 return false;
             } else if (ans == 1) {
                 return true;
             }
             n=ans;
         }
-    }
-
-    public static void main(String[] args) throws Exception {
-        System.out.println(new Leetcode_202().isHappy(7));
     }
 }
