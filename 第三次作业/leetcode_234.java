fd73d8db48782fe38e2933378594b7a8028d2a5b
javaLesson12 <2444574046@qq.com>
2015-10-09 22:50:54 +0800

    yu-1-1

--- 第三次作业/leetcode_234.java
+++ 第三次作业/leetcode_234.java
@@ -8,29 +8,65 @@
  */
 public class Solution {
     public boolean isPalindrome(ListNode head) {
-         if(head==null)
+        if (head == null)
             return true;
-        Stack<Integer> stack = new Stack<>();
+//        Stack<Integer> stack = new Stack<>();
+//        ListNode p = head;
+//        int len = 0;
+//        while (p != null) {
+//            len++;
+//            p = p.next;
+//        }
+//        int il = len / 2;
+//        p = head;
+//        while (il-->0) {
+//            stack.push(p.val);
+//            p = p.next;
+//        }
+//        System.out.println(p.val);
+//        if (len % 2 != 0) {
+//            p = p.next;
+//        }
+//        while (!stack.isEmpty()) {
+//            if (p.val != stack.pop())
+//                return false;
+//            p=p.next;
+//        }
         ListNode p = head;
         int len = 0;
         while (p != null) {
             len++;
             p = p.next;
         }
+        ListNode pre;
+        p=head;
         int il = len / 2;
-        p = head;
-        while (il-->0) {
-            stack.push(p.val);
+        while (il-- > 0) {
             p = p.next;
         }
-        System.out.println(p.val);
         if (len % 2 != 0) {
+            pre = p;
             p = p.next;
+        } else {
+            pre = null;
         }
-        while (!stack.isEmpty()) {
-            if (p.val != stack.pop())
+
+        ListNode next;
+        while (p != null) {
+            next = p.next;
+            p.next = pre;
+            pre = p;
+            p = next;
+        }
+        ListNode last = pre;
+        ListNode fst = head;
+        while (last != null && last != fst) {
+            if (fst.val != last.val) {
                 return false;
-            p=p.next;
+            }
+            fst = fst.next;
+            last = last.next;
+
         }
         return true;
     }
